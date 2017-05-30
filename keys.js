811a63f541ad1a6d70aa8dc87170b440480c488a
Anthony Morreale <anthony_morreale@dhcp-v069-181.mobile.uci.edu>
2017-05-30 13:51:32 -0700

    second upload

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'PHjFBtHjFm4QeEae2h2KHOFs8',
+  consumer_secret: 'plCy30iMyly4DZwDhiOGI0KCFKoHwAJp4rg7HONqq1bSCCzXXD',
+  access_token_key: '867590731229143040-e33mykhfEI5vgi8BsUkontItYSbStEn',
+  access_token_secret: 'KHizpI2BI1oi0g0SgDTrO1i4hKjucS69bwT3JI46BJWjY',
+}
